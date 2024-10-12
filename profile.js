d436d3689a4bab94b7f8bfacc255975d1f34b143
karan kumar <nolimetedmoney4@gmail.com>
2024-10-12 11:40:31 +0530

    add socal media

--- profile.js
+++ profile.js
@@ -1,7 +1,7 @@
 function showMore() {
-    var moreInfo = document.getElementById('more-info');
+    const moreInfo = document.getElementById('more-info');
     if (moreInfo.style.display === 'none') {
-        moreInfo.style.display = 'block';
+        moreInfo.style.display = 'flex';
     } else {
         moreInfo.style.display = 'none';
     }

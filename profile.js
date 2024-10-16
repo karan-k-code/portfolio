ea6d7b058c6b1e715900e0112eee08b6a367078a
karan kumar <nolimetedmoney4@gmail.com>
2024-10-16 07:29:13 +0530

    add tarnsition

--- profile.js
+++ profile.js
@@ -1,8 +1,27 @@
+ const showMoreBtn = document.getElementById("show_more_btn")
+
+ const moreInfo = document.getElementById('more-info');
+
 function showMore() {
-    const moreInfo = document.getElementById('more-info');
-    if (moreInfo.style.display === 'none') {
-        moreInfo.style.display = 'flex';
+    
+
+    // moreInfo.style.display= moreInfo.style.display === 'none'? 'block': 'none';
+    if (showMoreBtn.innerText=== 'Show More') {
+        showMoreBtn.innerText="Show Less";
+        moreInfo.classList.add('tran')
     } else {
-        moreInfo.style.display = 'none';
+        showMoreBtn.innerText="Show More";
+        moreInfo.classList.remove('tran')
     }
+    
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
